cf92d69d9b17bfc1fcae5209379c7ba5d88e1767
Nikita Belonogov <belonogovna@sgu.ru>
2017-10-17 14:16:03 +0400

    parallel

--- gitrecursive.sh
+++ gitrecursive.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
 home=$PWD
+FC=$'\033[30m'
+SC=$'\033[33m'
+NC=$'\033[0m'
+
+
+set -ue
 
 repositories_path=$(find $PWD -type directory -name '.git')
+
+function exec_command() {
+	local prefix=$1
+	local command=$2
+	result=$( git ${command} ) #| sed "s#^#${prefix} #")
+	echo -e ${prefix}$'\n'${result}
+	echo
+}
 
 for repo_git_path in $repositories_path; do
     repo_path=${repo_git_path%/*}
     repo_name=${repo_path##*/}
     repo_dir=${repo_path%/*}
     cd $repo_path
-    echo -e "\033[30m${repo_dir}/\033[33m${repo_name}\033[0m"
-    git $@
+    colored_path="${FC}${repo_dir}/${SC}${repo_name}${NC}"
+    exec_command ${colored_path} "$@" &
     cd $home
 done
 
+wait < <(jobs -p)
