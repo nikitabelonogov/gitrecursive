0e45dc7df5008c9b687d0a89a294dbf7249baa07
Nikita Belonogov <belonogovna@sgu.ru>
2018-07-06 13:23:43 +0400

    fix indent

--- gitrecursive.sh
+++ gitrecursive.sh
@@ -10,18 +10,18 @@ set -ue
 repositories_path=$(find ${PWD} -type directory -name '.git')
 
 function exec_command() {
-	local prefix=$1
-	local cmd=$2
-	echo -e "${prefix}\n$( ${cmd} )"
+  local prefix=$1
+  local cmd=$2
+  echo -e "${prefix}\n$( ${cmd} )"
 }
 
 for repo_git_path in $repositories_path; do
-    repo_path=${repo_git_path%/*}
-    repo_name=${repo_path##*/}
-    repo_dir=${repo_path%/*}
-    colored_path="${FC}${repo_dir}/${SC}${repo_name}${NC}"
-    cmd="git -C ${repo_path} ${@#*git}"
-    exec_command ${colored_path} "$cmd" &
+  repo_path=${repo_git_path%/*}
+  repo_name=${repo_path##*/}
+  repo_dir=${repo_path%/*}
+  colored_path="${FC}${repo_dir}/${SC}${repo_name}${NC}"
+  cmd="git -C ${repo_path} ${@#*git}"
+  exec_command ${colored_path} "$cmd" &
 done
 
 wait < <(jobs -p)
