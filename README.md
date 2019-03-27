778f5d187e8fb2252b543f465cf81f91c7ed85e3
Nikita <belonogovna@sgu.ru>
2019-03-27 15:01:22 +0400

    Minify

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This utility will apply the command to all child repositories.
 ## Installation
 * Clone the repo
 * Create a symlink in one of your PATHs
-  ```sh
-  ln -s -f $(pwd)/gitrecursive/gitrecursive.sh /usr/local/bin/gitrecursive
-  ```
+```sh
+ln -s -f $(pwd)/gitrecursive/gitrecursive.sh /usr/local/bin/gitrecursive
+```
 
 ## Usage
 ```sh
--- gitrecursive.sh
+++ gitrecursive.sh
@@ -7,22 +7,12 @@ NC=$'\033[0m'
 
 set -ue
 
-function exec_command() {
-  local prefix=$1
-  local cmd=$2
-  local stdout=$($cmd)
-  echo -e "${prefix}\n${stdout}"
-}
-
-repositories_path=$(find ${PWD} -type directory -name '.git')
-
-for repo_git_path in $repositories_path; do
+for repo_git_path in $(find ${PWD} -type directory -name '.git'); do
   repo_path=${repo_git_path%/*}
   repo_name=${repo_path##*/}
   colored_path=${repo_path/${PWD}/${FC}${PWD}${NC}}
   colored_path=${colored_path/${repo_name}/${SC}${repo_name}${NC}}
-  gitcmd=${@#*git}
-  exec_command "${colored_path} ${gitcmd}" "git -C ${repo_path} ${gitcmd}" &
+  echo -e "${colored_path} $@\n$(git -C ${repo_path} $@)" &
 done
 
 wait < <(jobs -p)
