92dda8bd00dd39d37346094faa29fb6290744dc0
Nikita <belonogovna@sgu.ru>
2019-09-05 20:26:02 +0400

    small update

--- gitrecursive.sh
+++ gitrecursive.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+set -ue ${DEBUG:+-x}
+
 FC=$'\033[30m'
 SC=$'\033[33m'
 NC=$'\033[0m'
-
-
-set -ue
 
 for repo_git_path in $(find ${PWD} -type directory -name '.git'); do
   repo_path=${repo_git_path%/*}
@@ -15,4 +14,4 @@ for repo_git_path in $(find ${PWD} -type directory -name '.git'); do
   echo -e "${colored_path} $@\n$(git -C ${repo_path} $@)" &
 done
 
-wait < <(jobs -p)
+wait
