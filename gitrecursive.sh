af66e2f5d87abcf79e7a8eb9777bc35aeb351842
Nikita Belonogov <waksaone@gmail.com>
2026-03-24 16:01:13 +0400

    Add optional depth argument to limit recursive Git operations

--- gitrecursive.sh
+++ gitrecursive.sh
@@ -6,12 +6,30 @@ FC=$'\033[30m'
 SC=$'\033[33m'
 NC=$'\033[0m'
 
-for repo_git_path in $(find ${PWD} -type directory -name '.git'); do
+depth_args=()
+args=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d|--depth)
+      depth_args=(-maxdepth "$(($2 + 1))")
+      shift 2
+      ;;
+    *)
+      args+=("$1")
+      shift
+      ;;
+  esac
+done
+set -- "${args[@]+"${args[@]}"}"
+
+for repo_git_path in $(find ${PWD} "${depth_args[@]+"${depth_args[@]}"}" -type directory -name '.git'); do
   repo_path=${repo_git_path%/*}
   repo_name=${repo_path##*/}
   colored_path=${repo_path/${PWD}/${FC}${PWD}${NC}}
   colored_path=${colored_path/${repo_name}/${SC}${repo_name}${NC}}
-  echo -e "${colored_path} $*\n$(git -C ${repo_path} $*)" &
+  echo "${colored_path}"
+  git -C "${repo_path}" "$@" || true
+  echo "---"
 done
 
 wait
